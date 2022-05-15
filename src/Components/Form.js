cc832fa95766c59ff86c59a53f5509dd3b5ba11e
dazzlerabhi30800 <abhisingh30800@gmail.com>
2022-05-15 11:32:13 +0530

    Forms in react: Checkbox

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,24 +6,26 @@ function Form() {
     lastName: "",
     email: "",
     comments: "",
+    isFriendly: false,
   });
 
   function handleChange(event) {
+    const {name, value, type, checked} = event.target
     setFormData((prevFormData) => {
       return {
         ...prevFormData,
-        [event.target.name]: event.target.value,
+        [name]: type === "checkbox" ? checked : value
       };
     });
   }
 
   return (
-    <form className="grid gap-2 w-80">
+    <form className="gap-2">
       <input
         type="text"
         placeholder="First Name"
         onChange={handleChange}
-        className="mx-2 p-2 rounded-md"
+        className="m-2 p-2 rounded-md"
         name="firstName"
         value={formData.firstName}
       />
@@ -31,14 +33,14 @@ function Form() {
         type="text"
         placeholder="Last Name"
         onChange={handleChange}
-        className="mx-2 p-2 rounded-md"
+        className="m-2 p-2 rounded-md"
         name="lastName"
         value={formData.lastName}
       />
       <input
         type="email"
         placeholder="Please enter your email"
-        className="mx-2 p-2 rounded-md"
+        className="m-2 p-2 rounded-md"
         name="email"
         onChange={handleChange}
         value={formData.email}
@@ -46,11 +48,21 @@ function Form() {
       <textarea
         onChange={handleChange}
         name="comments"
-        className="mx-2 p-2 rounded-md"
+        className="mx- p-2 rounded-md"
         value={formData.comments}
       >
         Hello this is a textarea
       </textarea>
+      <input
+        type="checkbox"
+        id="isFriendly"
+        onChange={handleChange}
+        checked={formData.isFriendly}
+        className="mx-2"
+        name="isFriendly"
+       />
+       <label htmlFor="isFriendly">Are you Friendly ?</label>
+       <br />
     </form>
   );
 }
