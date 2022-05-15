e31de90f8545dda34e8f3ccb0525195c4ba02aaa
dazzlerabhi30800 <abhisingh30800@gmail.com>
2022-05-15 11:19:12 +0530

    Forms in react: textarea

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,25 +5,25 @@ function Form() {
     firstName: "",
     lastName: "",
     email: "",
+    comments: "",
   });
 
   function handleChange(event) {
-    setFormData(prevFormData => {
-       return {
-            ...prevFormData,
-            [event.target.name]: event.target.value
-       } 
-    })
-  } 
-
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
 
   return (
-    <form className="grid gap-2 w-60">
+    <form className="grid gap-2 w-80">
       <input
         type="text"
         placeholder="First Name"
         onChange={handleChange}
-        className="mx-2 px-2 rounded-md py-2"
+        className="mx-2 p-2 rounded-md"
         name="firstName"
         value={formData.firstName}
       />
@@ -31,18 +31,26 @@ function Form() {
         type="text"
         placeholder="Last Name"
         onChange={handleChange}
-        className="mx-2 px-2 rounded-md py-2"
+        className="mx-2 p-2 rounded-md"
         name="lastName"
         value={formData.lastName}
       />
       <input
         type="email"
         placeholder="Please enter your email"
-        className="mx-2 px-2 rounded-md py-2"
+        className="mx-2 p-2 rounded-md"
         name="email"
         onChange={handleChange}
         value={formData.email}
-       />
+      />
+      <textarea
+        onChange={handleChange}
+        name="comments"
+        className="mx-2 p-2 rounded-md"
+        value={formData.comments}
+      >
+        Hello this is a textarea
+      </textarea>
     </form>
   );
 }
