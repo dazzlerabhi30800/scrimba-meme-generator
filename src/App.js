22bf49670ef6d52b1235ce85f07f9bf50599835a
= <abhisingh30800@gmail.com>
2022-05-16 11:05:53 +0530

    Please work this time

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
@@ -14,19 +13,6 @@ import jokesData from "./Components/JokesData";
 import Form from './Components/Form'
 
 function App() {
-=======
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
-import Meme from './Components/Meme'
-import PicSum from './Components/PicSum'
-import Connundrum from './Components/Connundrum'
-import Card from './Components/Card'
-import React, {useState} from 'react';
-import boxes from './boxes';
-import Box from './Components/Box'
-import Form from './Components/Form'
-function App() {
 
   const [squares, setSquares] = useState(boxes);
 
@@ -35,15 +21,10 @@ function App() {
   }
 
 
-
-
-
-
   const squareElements = squares.map(square => (
     <Box on={square.on} key={square.id} />
   ))
 
->>>>>>> 67db59a (Added git in windows)
   return (
     <div className="App">
       {/* <Header /> */}
