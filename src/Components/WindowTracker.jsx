b2a566ea5734f24324cff27fd659400420408108
Abhishek <abhishek@pop-os.localdomain>
2022-05-17 19:20:46 +0530

    Fixed memory leak using Clean up function

--- src/Components/WindowTracker.jsx
+++ src/Components/WindowTracker.jsx
@@ -6,10 +6,16 @@ function WindowTracker() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
     useEffect(() => {
-        window.addEventListener('resize', function() {
+        function watchWidth() {
             setWindowWidth(window.innerWidth);
-        })
-    })
+        }
+        window.addEventListener('resize', watchWidth)
+
+        return function() {
+            console.log("cleaning up....")
+            window.removeEventListener('resize', watchWidth)
+        }
+}, [])
     return(
         <h1>Window width : {window.innerWidth}</h1>
     )
